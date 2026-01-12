2c2c1429baef05cfcca1493ec5bfbbf786aba86e
Sriram R <150992712+sriram36505@users.noreply.github.com>
2026-01-12 20:11:40 +0530

    Add dataset_loader.py - Data loading and transformation utilities

---
+++ src/dataset_loader.py
@@ -0,0 +1,39 @@
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+def get_transforms(img_size=224):
+    train_tf = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+    test_tf = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+    return train_tf, test_tf
+
+def load_dataset(root_dir, batch_size=32):
+    train_tf, test_tf = get_transforms()
+
+    full_dataset = datasets.ImageFolder(root_dir, transform=train_tf)
+
+    train_size = int(0.7 * len(full_dataset))
+    val_size = int(0.15 * len(full_dataset))
+    test_size = len(full_dataset) - train_size - val_size
+
+    train_ds, val_ds, test_ds = random_split(
+        full_dataset, [train_size, val_size, test_size]
+    )
+
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader, full_dataset.classes
